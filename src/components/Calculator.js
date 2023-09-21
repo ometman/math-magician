eca785966142973996c72d8e926d018fd1a5b8c0
ometman <revomet@gmail.com>
2023-09-21 04:52:28 +0000

    Implement math magician even logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,69 @@
-import TheScreen from './Screen';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Calculate from '../logic/Calculate';
 
-function Btn(prop) {
-  const { name } = prop;
+function TheDisplay({ outputVal }) {
   return (
-    <button type="button" className="square">
-      { name }
-    </button>
+    <div className="display">
+      <input
+        className="calc-input"
+        value={outputVal}
+      />
+    </div>
   );
 }
 
-function BtnSign(prop) {
-  const { name } = prop;
-  return (
-    <button type="button" className="square orange">
-      { name }
-    </button>
-  );
-}
-
-function BtnZero(prop) {
-  const { name } = prop;
-  return (
-    <button type="button" className="square d-width">
-      { name }
-    </button>
-  );
-}
+TheDisplay.propTypes = {
+  outputVal: PropTypes.string.isRequired,
+};
 
 function Calculator() {
+  const [valObj, setValObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    const inputValue = e.target.value;
+    setValObj(Calculate(valObj, inputValue));
+  };
+
   return (
     <div className="main-rows">
-      <TheScreen />
+      <TheDisplay
+        outputVal={
+          valObj.next ? valObj.next : valObj.total || 0
+        }
+      />
       <div className="Btn-row">
-        <Btn value="AC" name="AC" />
-        <Btn value="+/-" name="+/-" />
-        <Btn value="%" name="%" />
-        <BtnSign className="dot" value="/" name="&divide;" />
+        <button type="button" className="square" value="AC" onClick={handleClick}>AC</button>
+        <button type="button" className="square" value="+/-" onClick={handleClick}>+/-</button>
+        <button type="button" className="square" value="%" onClick={handleClick}>%</button>
+        <button type="button" className="square orange" value="/" onClick={handleClick}>&divide;</button>
       </div>
       <div className="Btn-row">
-        <Btn value="7" name="7" />
-        <Btn value="8" name="8" />
-        <Btn value="9" name="9" />
-        <BtnSign value="x" name="x" />
+        <button type="button" className="square" value="7" onClick={handleClick}>7</button>
+        <button type="button" className="square" value="8" onClick={handleClick}>8</button>
+        <button type="button" className="square" value="9" onClick={handleClick}>9</button>
+        <button type="button" className="square orange" value="x" onClick={handleClick}>&times;</button>
       </div>
       <div className="Btn-row">
-        <Btn value="4" name="4" />
-        <Btn value="5" name="5" />
-        <Btn value="6" name="6" />
-        <BtnSign value="-" name="-" />
+        <button type="button" className="square" value="4" onClick={handleClick}>4</button>
+        <button type="button" className="square" value="5" onClick={handleClick}>5</button>
+        <button type="button" className="square" value="6" onClick={handleClick}>6</button>
+        <button type="button" className="square orange" value="-" onClick={handleClick}>&minus;</button>
       </div>
       <div className="Btn-row">
-        <Btn value="1" name="1" />
-        <Btn value="2" name="2" />
-        <Btn value="3" name="3" />
-        <BtnSign value="+" name="+" />
+        <button type="button" className="square" value="1" onClick={handleClick}>1</button>
+        <button type="button" className="square" value="2" onClick={handleClick}>2</button>
+        <button type="button" className="square" value="3" onClick={handleClick}>3</button>
+        <button type="button" className="square orange" value="+" onClick={handleClick}>&#43;</button>
       </div>
       <div className="Btn-row">
-        <BtnZero value="0" name="0" />
-        <Btn value="&#729;" name="&middot;" />
-        <BtnSign value="&#61;" name="&#61;" />
+        <button type="button" className="square d-width" value="0" onClick={handleClick}>0</button>
+        <button type="button" className="square" value="." onClick={handleClick}>&middot;</button>
+        <button type="button" className="square orange" value="&#61;" onClick={handleClick}>&#61;</button>
       </div>
     </div>
   );
